6d0835a7cd1aa6b6017c6b0fae2fbfdce0b257a2
NamazovMN <61018245+NamazovMN@users.noreply.github.com>
2021-07-18 15:24:25 +0200

    Add files via upload

--- EAI_vision/main.py
+++ EAI_vision/main.py
@@ -1,36 +1,35 @@
 import mtcnn
 from tensorflow.keras.models import load_model
 import dlib
-from check import CheckGlasses
-from database import Credentials
-import cv2
+
 import os
 import pickle
 from utils import *
 from vision import Vision
 from database import Credentials
 from check import CheckGlasses
-path = "data/shape_predictor_5_face_landmarks.dat"
 database_path = 'Database'
+encodings_dict_path = os.path.join(database_path, 'encodings.pkl')
 database_images = os.path.join(database_path, 'people')
 database_names_path = os.path.join(database_path, 'usernames.txt')
-
-# if not os.path.exists(database_names_path):
-#     os.makedirs(database_names_path)
-cap = cv2.VideoCapture(0)
-predictor = dlib.shape_predictor(path)
-detector = dlib.get_frontal_face_detector()
-credentials = Credentials(database_names_path)
+shape_predictor_path = "data/shape_predictor_5_face_landmarks.dat"
 encoder_model = './facenet_keras/data/model/facenet_keras.h5'
-encodings_path = './facenet_keras/data/encodings/encodings.pkl'
-encode_new = os.path.join(database_path, 'encodings.pkl')
+# encodings_path = './facenet_keras/data/encodings/encodings.pkl'
+
+
+
+cap = cv2.VideoCapture(0)
+shape_predictor = dlib.shape_predictor(shape_predictor_path)
+frontal_face_detector = dlib.get_frontal_face_detector()
+credentials = Credentials(database_names_path)
+
 
 face_detector = mtcnn.MTCNN()
 face_encoder = load_model(encoder_model)
-encoding_dict = load_pickle(encodings_path)
+# encoding_dict = load_pickle(encodings_dict_path)
 
-checker = CheckGlasses(path)
-vision = Vision(database_path, database_images, cap, checker, detector, predictor, face_encoder, face_detector, encoding_dict, encodings_path)
+checker = CheckGlasses(shape_predictor_path)
+vision = Vision(credentials, database_path, database_images, cap, checker, frontal_face_detector, shape_predictor, face_encoder, face_detector, encodings_dict_path)
 
 # print(vision.encode_dict)
 # vision.handle_cv(True)
